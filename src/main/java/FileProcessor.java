edf6352df9928f62058f4a93d1dbbcea8159fcd2
Dgray16 <vova235@gmail.com>
2017-07-18 22:13:57 +0300

    Add spacing for logger pattern. Add calculation of some function parameters.

--- src/main/java/FileProcessor.java
+++ src/main/java/FileProcessor.java
@@ -3,6 +3,7 @@ import ch.qos.logback.core.encoder.ByteArrayUtil;
 import lombok.SneakyThrows;
 
 import java.io.RandomAccessFile;
+import java.math.BigDecimal;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Arrays;
@@ -10,26 +11,26 @@ import java.util.Arrays;
 
 public class FileProcessor {
 
+    /** Length of file */
+    private static final int length = 126;
+
     /**
      * {@link RandomAccessFile#length()} returns value measure in bytes!
      * 126 bytes == 1008 bits
      */
     @SneakyThrows
-    public RandomAccessFile generateFile() {
-        RandomAccessFile randomAccessFile = new RandomAccessFile("idea-cipher", "rw");
-        randomAccessFile.setLength(4);
+    public RandomAccessFile generateFile(String filename) {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(filename, "rw");
+        randomAccessFile.setLength(length);
 
-        byte[] zeroArray = generateByteArray(2, 0);
-        byte[] unitArray = generateByteArray(2, 1);
+        byte[] zeroArray = generateByteArray(length / 2, 0);
+        byte[] unitArray = generateByteArray(length / 2, 1);
 
         write(randomAccessFile, zeroArray, 0);
-
-        randomAccessFile = new RandomAccessFile("idea-cipher", "rw");
-        byte[] result = read(randomAccessFile);
-
-        /* TODO everything is ready to feed file with data */
+        write(randomAccessFile, unitArray, length / 2);
 
         Main.LOG.info("File generated: {}", randomAccessFile);
+
         return randomAccessFile;
     }
 
@@ -37,11 +38,10 @@ public class FileProcessor {
     private void write(RandomAccessFile file, byte[] array, int startPosition) {
         file.seek(startPosition);
         file.write(array);
-        file.close();
     }
 
     @SneakyThrows
-    private byte[] read(RandomAccessFile file) {
+    public byte[] read(RandomAccessFile file) {
         file.seek(0);
         byte[] data = new byte[(int) file.length()];
         file.readFully(data);
@@ -52,6 +52,11 @@ public class FileProcessor {
         byte[] array = new byte[arraySize];
         Arrays.fill(array, (byte) value);
         return array;
+    }
+
+    @SneakyThrows
+    public BigDecimal getFileLength(RandomAccessFile file) {
+        return BigDecimal.valueOf(file.length());
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.RandomAccessFile;
+import java.math.BigDecimal;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 
 /**
@@ -12,8 +15,23 @@ public class Main {
     public static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+
+        /* Ideal cipher */
         FileProcessor fileProcessor = new FileProcessor();
-        RandomAccessFile file = fileProcessor.generateFile();
+        RandomAccessFile file = fileProcessor.generateFile("idea-cipher");
+        byte[] result = fileProcessor.read(file);
+
+        /* Workaround to reuse stream */
+        Supplier<IntStream> intStream = () -> IntStream.range(0, result.length).map(i -> result[i]);
+
+        BigDecimal zerosCount = BigDecimal.valueOf(intStream.get().filter(value -> value == 0).count());
+        BigDecimal unitsCount = BigDecimal.valueOf(intStream.get().filter(value -> value == 1).count());
+
+        LOG.info(
+                "P1 = zeroCount / length = {} / {} = {}",
+                zerosCount, fileProcessor.getFileLength(file), zerosCount.divide(fileProcessor.getFileLength(file)));
+
+        /* TODO get statistics for idea cipher */
     }
 
 }
