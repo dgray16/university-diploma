14d44b2bcece323a32ac25407d3482f59dbf46f2
Dgray16 <vova235@gmail.com>
2017-09-10 12:04:43 +0300

    Add info link.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,6 +29,7 @@ import static org.apache.commons.lang3.StringUtils.EMPTY;
  *     <li>https://ru.wikipedia.org/wiki/%D0%A1%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B5_%D1%82%D0%B5%D1%81%D1%82%D1%8B_NIST</li>
  *     <li>http://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-22r1a.pdf</li>
  *     <li>https://github.com/kravietz/nist-sts</li>
+ *     <li>https://ru.wikipedia.org/wiki/%D0%A4%D0%BE%D1%80%D0%BC%D1%83%D0%BB%D0%B0_%D0%A5%D0%B0%D1%80%D1%82%D0%BB%D0%B8</li>
  * </ul>
  *
  Test text data:
